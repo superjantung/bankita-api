5eb5a9882a3c718a857ec98cc207cc7a12a70493
Fauzan Fathurrahman <fauzan08fauzan@gmail.com>
2023-06-03 20:30:30 +0700

    add randomizer

---
+++ util/random.go
@@ -0,0 +1,42 @@
+package util
+
+import (
+	"math/rand"
+	"strings"
+	"time"
+)
+
+const alphabet = "abcdefghijklmnopqrstuvwxyz"
+
+func init() {
+	rand.Seed(time.Now().UnixNano())
+}
+
+func RandomInt64(min, max int64) int64 {
+	return min + rand.Int63n(max-min+1)
+}
+
+func RandomString(n int) string {
+	var sb strings.Builder
+	sb.Grow(n)
+
+	for i := 0; i < n; i++ {
+		sb.WriteByte(alphabet[rand.Intn(len(alphabet))])
+	}
+
+	return sb.String()
+}
+
+func RandomOwner() string {
+	return RandomString(6)
+}
+
+func RandomBalance() int64 {
+	return RandomInt64(0, 10000000)
+}
+
+func RandomCurrencies() string {
+	currencies := []string{"USD", "EUR", "IDR"}
+	n := len(currencies)
+	return currencies[rand.Intn(n)]
+}
