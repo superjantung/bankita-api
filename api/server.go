60d508fb54d2eb6a8fdfde787a1f65c24fcd936d
Fauzan Fathurrahman <fauzan08fauzan@gmail.com>
2023-06-21 07:08:54 +0700

    start server

---
+++ api/server.go
@@ -0,0 +1,32 @@
+package api
+
+import (
+	"github.com/gin-gonic/gin"
+	db "github.com/superjantung/bankita-api/db/sqlc"
+)
+
+type Server struct {
+	store  *db.Store
+	router *gin.Engine
+}
+
+func NewServer(store *db.Store) *Server {
+	server := &Server{store: store}
+	router := gin.Default()
+
+	api := router.Group("/api")
+	{
+		api.POST("/accounts", server.createAccount)
+	}
+
+	server.router = router
+	return server
+}
+
+func (server *Server) Start(address string) error {
+	return server.router.Run(address)
+}
+
+func errorResponse(err error) gin.H {
+	return gin.H{"error": err.Error()}
+}
