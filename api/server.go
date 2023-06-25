4a6b5b51c34b3be765ca39f29a3cd5c3a7d0d950
Fauzan Fathurrahman <fauzan08fauzan@gmail.com>
2023-06-25 11:20:45 +0700

    add transfer api

--- api/server.go
+++ api/server.go
@@ -2,6 +2,8 @@ package api
 
 import (
 	"github.com/gin-gonic/gin"
+	"github.com/gin-gonic/gin/binding"
+	"github.com/go-playground/validator/v10"
 	db "github.com/superjantung/bankita-api/db/sqlc"
 )
 
@@ -14,11 +16,17 @@ func NewServer(store db.Store) *Server {
 	server := &Server{store: store}
 	router := gin.Default()
 
+	if v, ok := binding.Validator.Engine().(*validator.Validate); ok {
+		v.RegisterValidation("currency", validCurrency)
+	}
+
 	api := router.Group("/api")
 	{
 		api.POST("/accounts", server.createAccount)
 		api.GET("/accounts/:id", server.getAccount)
 		api.GET("/accounts", server.listAccount)
+
+		api.POST("/transfers", server.createTransfer)
 	}
 
 	server.router = router
